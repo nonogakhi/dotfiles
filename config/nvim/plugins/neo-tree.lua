3ae4d39f1749b5f97a9df53cad39edd9ba1cb0bc
Thieu Nguyen <thieu.nguyen@oivan.com>
2022-05-07 01:03:35 +0700

    Fix: Redraw alpha when open neo-tree window

--- config/nvim/plugins/neo-tree.lua
+++ config/nvim/plugins/neo-tree.lua
@@ -5,4 +5,17 @@ return {
     position = "right",
     width = 35,
   },
+  event_handlers = {
+    {
+      event = "after_render",
+      handler = function(_)
+        if vim.bo.filetype == "neo-tree" then
+          local status_ok, alpha = pcall(require, "alpha")
+          if status_ok then
+            alpha.redraw()
+          end
+        end
+      end,
+    },
+  },
 }
