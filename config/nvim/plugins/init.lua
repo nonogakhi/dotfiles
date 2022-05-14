1bc552c01a292a4ff968d153612e448e45f71fb0
Thieu Nguyen <thieu.nguyen@oivan.com>
2022-05-14 16:08:55 +0700

    refactor

--- config/nvim/plugins/init.lua
+++ config/nvim/plugins/init.lua
@@ -4,18 +4,21 @@ return {
   ["max397574/better-escape.nvim"] = { disable = true },
   ["lukas-reineke/indent-blankline.nvim"] = { disable = true },
   ["numToStr/Comment.nvim"] = { disable = true },
-  {
-    "EdenEast/nightfox.nvim", -- colorscheme/theme
+
+  -- Colorscheme/theme
+  ["EdenEast/nightfox.nvim"] = {
     config = function()
       require("nightfox").setup(require "user.plugins.nightfox")
     end,
   },
-  {
-    "yamatsum/nvim-nonicons", -- font for icons
+
+  -- Font for icons
+  ["yamatsum/nvim-nonicons"] = {
     requires = { "kyazdani42/nvim-web-devicons" },
   },
-  {
-    "danymat/neogen", -- annotation generator
+
+  -- Annotation generator
+  ["danymat/neogen"] = {
     module = "neogen",
     cmd = "Neogen",
     config = function()
@@ -23,69 +26,71 @@ return {
     end,
     requires = "nvim-treesitter/nvim-treesitter",
   },
-  {
-    "andymass/vim-matchup", -- highlight, navigate, and operate on sets of matching text
+
+  -- Highlight, navigate, and operate on sets of matching text
+  ["andymass/vim-matchup"] = {
     after = "nvim-treesitter",
-    setup = function()
-      require("core.utils").defer_plugin "vim-matchup"
-    end,
   },
-  {
-    "echasnovski/mini.nvim", -- collection of minimal, independent, and fast Lua modules
-    opt = true,
-    setup = function()
-      require("core.utils").defer_plugin "mini.nvim"
-    end,
+
+  -- Collection of minimal, independent, and fast Lua modules
+  ["echasnovski/mini.nvim"] = {
+    event = "VimEnter",
     config = function()
       require "user.plugins.mini"()
     end,
   },
-  {
-    "ellisonleao/glow.nvim", -- markdown preview
+
+  -- Markdown preview
+  ["ellisonleao/glow.nvim"] = {
     cmd = "Glow",
     module = "glow",
     setup = function()
       vim.g.glow_border = "rounded"
     end,
   },
-  {
-    "ethanholz/nvim-lastplace", -- reopen files at last edit position
+
+  -- Reopen files at last edit position
+  ["ethanholz/nvim-lastplace"] = {
     event = "BufRead",
     config = function()
       require("nvim-lastplace").setup(require "user.plugins.nvim-lastplace")
     end,
   },
-  {
-    "folke/zen-mode.nvim",
+
+  ["folke/zen-mode.nvim"] = {
     cmd = "ZenMode",
     module = "zen-mode",
     config = function()
       require("zen-mode").setup(require "user.plugins.zen-mode")
     end,
   },
-  {
-    "hrsh7th/cmp-emoji",
+
+  ["hrsh7th/cmp-emoji"] = {
     after = "nvim-cmp",
     config = function()
-      require("core.utils").add_user_cmp_source "emoji"
+      astronvim.add_user_cmp_source "emoji"
     end,
   },
-  {
-    "lukas-reineke/headlines.nvim", -- highlights for text filetypes, like markdown
+
+  -- Highlights for text filetypes, like markdown
+  ["lukas-reineke/headlines.nvim"] = {
     ft = { "markdown", "rmd" },
     config = function()
       require("headlines").setup(require "user.plugins.headlines")
     end,
   },
-  {
-    "mickael-menu/zk-nvim", -- plain text note-taking assistant
+
+  -- plain text note-taking assistant
+  ["mickael-menu/zk-nvim"] = {
     module = { "zk", "zk.commands" },
     config = function()
       require("zk").setup(require "user.plugins.zk")
     end,
   },
-  {
-    "phaazon/hop.nvim",
+
+  ["nanotee/sqls.nvim"] = { module = "sqls" },
+
+  ["phaazon/hop.nvim"] = {
     branch = "v1",
     cmd = {
       "HopChar1CurrentLine",
@@ -95,38 +100,39 @@ return {
       require("hop").setup()
     end,
   },
-  { "nvim-treesitter/nvim-treesitter-textobjects", after = "nvim-treesitter" },
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
+  ["nvim-treesitter/nvim-treesitter-textobjects"] = { after = "nvim-treesitter" },
+
+  ["nvim-telescope/telescope-file-browser.nvim"] = {
     after = "telescope.nvim",
-    module = "telescope._extensions.file_browser", -- lazy-load
+    module = "telescope._extensions.file_browser",
     config = function()
       require("telescope").load_extension "file_browser"
     end,
   },
-  {
-    "nvim-telescope/telescope-hop.nvim",
+
+  ["nvim-telescope/telescope-hop.nvim"] = {
     after = "telescope.nvim",
-    module = "telescope._extensions.hop", -- lazy-load
+    module = "telescope._extensions.hop",
     config = function()
       require("telescope").load_extension "hop"
     end,
   },
-  {
-    "nvim-telescope/telescope-project.nvim",
+
+  ["nvim-telescope/telescope-project.nvim"] = {
     after = "telescope.nvim",
-    module = "telescope._extensions.project", -- lazy-load
+    module = "telescope._extensions.project",
     config = function()
       require("telescope").load_extension "project"
     end,
   },
-  {
-    "benfowler/telescope-luasnip.nvim",
+
+  ["benfowler/telescope-luasnip.nvim"] = {
     after = "telescope.nvim",
-    module = "telescope._extensions.luasnip", -- lazy-load
+    module = "telescope._extensions.luasnip",
     config = function()
       require("telescope").load_extension "luasnip"
     end,
   },
-  { "ziontee113/syntax-tree-surfer", module = "syntax-tree-surfer" },
+
+  ["ziontee113/syntax-tree-surfer"] = { module = "syntax-tree-surfer" },
 }
